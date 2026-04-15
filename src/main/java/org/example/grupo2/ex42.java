dd7c3fc6043390dd61f678d6f5082bcae771f693
gustamuniz7 <gustavo.doliveira@sptech.school>
2026-04-15 14:28:22 -0300

    Grupo 2 finalizado

--- src/main/java/org/example/grupo2/ex42.java
+++ src/main/java/org/example/grupo2/ex42.java
@@ -9,16 +9,45 @@ public class ex42 {
 
                 Random random = new Random();
                 int totalGrupos = 40;
-                int numerosPorGrupo = 3;
 
                 for (int i = 1; i <= totalGrupos; i++) {
-                    TreeSet<Integer> grupo = new TreeSet<>();
-                    while (grupo.size() < numerosPorGrupo) {
-                        grupo.add(random.nextInt(60)); // Sorteia de 0 a 59
-                    }
+
+                    int n1;
+                    int n2;
+                    int n3;
+
+                    n1 = random.nextInt(60);
+
+                    do {
+                        n2 = random.nextInt(60);
+                    } while (n2 == n1);
+
+                    do {
+                        n3 = random.nextInt(60);
+                    }while (n3 == n1 || n3 == n2);
+
+                   int ordenar;
+                   if (n1 > n2){
+                       ordenar = n1;
+                       n1 = n2;
+                       n2 = ordenar;
+                   }
+
+                   if (n1 > n3){
+                       ordenar = n1;
+                       n1 = n3;
+                       n3 = ordenar;
+                   }
+
+                   if (n2 > n3){
+                       ordenar = n2;
+                       n2 = n3;
+                       n3 = ordenar;
+                   }
 
 
-                    System.out.println("Grupo " + i + ": " + grupo);
+
+                    System.out.println("Grupo " + i + ": " + n1 + " " + n2 + " " + n3);
                 }
 
 
